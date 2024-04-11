83864e73bbeb6ded5156a0553e715df22c972d29
vishnuvardhan481999 <160810215+vishnuvardhan481999@users.noreply.github.com>
2024-04-11 13:39:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+setTimeout(() => {
+    document.getElementById("display").innerText = "10";
+    setTimeout(() => {
+      document.getElementById("display").innerText = "9";
+      setTimeout(() => {
+        document.getElementById("display").innerText = "8";
+        setTimeout(() => {
+          document.getElementById("display").innerText = "7";
+          setTimeout(() => {
+            document.getElementById("display").innerText = "6";
+            setTimeout(() => {
+              document.getElementById("display").innerText = "5";
+              setTimeout(() => {
+                document.getElementById("display").innerText = "4";
+                setTimeout(() => {
+                  document.getElementById("display").innerText = "3";
+                  setTimeout(() => {
+                    document.getElementById("display").innerText = "2";
+                    setTimeout(() => {
+                      document.getElementById("display").innerText = "1";
+                      setTimeout(() => {
+                        document.getElementById("display").innerText =
+                          "Happy New Year";
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
